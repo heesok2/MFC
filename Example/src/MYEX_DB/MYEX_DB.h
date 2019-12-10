ecb423fd7fb780d6d836b2166291eecf6428383b
heesok2 <heesok2@gmail.com>
2019-12-10 13:50:17 +0900

    * MYEX DB 프로젝트 추가

---
+++ Example/src/MYEX_DB/MYEX_DB.h
@@ -0,0 +1,27 @@
+﻿// MYEX_DB.h : MYEX_DB DLL의 주 헤더 파일
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CMYEXDBApp
+// 이 클래스 구현에 대해서는 MYEX_DB.cpp를 참조하세요.
+//
+
+class CMYEXDBApp : public CWinApp
+{
+public:
+	CMYEXDBApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
