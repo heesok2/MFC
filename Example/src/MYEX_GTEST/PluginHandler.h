448449bbc1b299076c84874fd9bd41dbe422d37d
jhs0215 <jhs0215@midasit.com>
2019-12-30 16:51:48 +0900

    * 불필요한 프로젝트 제거

--- Example/src/MYEX_GTEST/PluginHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef DEF_GTEST
-#define DEF_GTEST
-
-extern "C" AFX_EXT_API BOOL ReqService(LPCTSTR strArguments, LPVOID lpInitData);
-
-#endif // !DEF_GTEST
--- Example/src/MYEX_UI/PluginHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef DEF_PLUGIN_UI
-#define DEF_PLUGIN_UI
-
-extern "C" AFX_EXT_API BOOL ReqService(LPCTSTR strArguments, LPVOID lpInitData);
-
-#endif // !DEF_PLUGIN_UI
